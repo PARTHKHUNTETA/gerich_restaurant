91fccc1a713b24b62005c02b65561542d66a9c65
Parth Khunteta <parthkhunteta@Parths-MacBook-Air.local>
2024-01-10 15:30:29 +0530

    feat(): restraunt website

---
+++ src/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
